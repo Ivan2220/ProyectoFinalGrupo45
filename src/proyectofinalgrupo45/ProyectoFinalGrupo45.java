3e45dc0612947ea062453dd287ccc1b6cfa832ee
Ivan2220 <albornozivan15@gmail.com>
2023-10-03 17:31:40 -0300

    main

--- src/proyectofinalgrupo45/ProyectoFinalGrupo45.java
+++ src/proyectofinalgrupo45/ProyectoFinalGrupo45.java
@@ -1,21 +1,14 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package proyectofinalgrupo45;
 
-/**
- *
- * @author HP
- */
+import java.sql.Connection;
+import proyectofinalgrupo45.accesoADatos.Conexion;
+
 public class ProyectoFinalGrupo45 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        Connection con = Conexion.getConexion();
+
     }
-    
+
 }
