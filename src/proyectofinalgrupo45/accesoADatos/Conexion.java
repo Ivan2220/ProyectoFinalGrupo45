6cc4fe80f4dd6be6548bc4a822ec91104d3e2d75
sorona3 <nancysorondo@gmail.com>
2023-10-05 16:52:58 -0300

    lectordata

--- src/proyectofinalgrupo45/accesoADatos/Conexion.java
+++ src/proyectofinalgrupo45/accesoADatos/Conexion.java
@@ -9,7 +9,7 @@ import javax.swing.JOptionPane;
 
 public class Conexion {
     
-     private static final String URL = "jdbc:mariadb://localhost:3306/";
+    private static final String URL = "jdbc:mariadb://localhost:3306/";
     private static final String DB = "sistemadebiblioteca";
     private static final String USUARIO = "root";
     private static final String PASSWORD = "";
---
+++ src/proyectofinalgrupo45/accesoADatos/LectorData.java
@@ -0,0 +1,180 @@
+
+package proyectofinalgrupo45.accesoADatos;
+
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.JOptionPane;
+import proyectofinalgrupo45.entidades.Lector;
+
+
+public class LectorData {
+    
+    private Connection con = null;
+
+    public LectorData() {
+        
+         con = Conexion.getConexion();
+    }
+    
+    public void guardarLector(Lector lector) {
+
+        String sql = "INSERT INTO lector (dni, nombre,domicilio,telefono, estado)"
+                + "VALUES(?, ?, ?, ?, ?)";
+
+        try {
+            PreparedStatement ps = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            ps.setInt(1, lector.getDni());
+            ps.setString(2, lector.getNombre());
+            ps.setString(3, lector.getDomicilio());
+            ps.setInt(4,lector.getTelefono());
+            ps.setBoolean(5,lector.isEstado());// ojo no me da la palabra activo
+            ps.executeUpdate();
+
+            ResultSet rs = ps.getGeneratedKeys();
+
+            if (rs.next()) {
+
+                lector.setIdLector(rs.getInt(1));
+                JOptionPane.showMessageDialog(null, "Lector Guardado");
+
+            }
+            
+            ps.close();
+
+        } catch (SQLException ex) {
+
+            JOptionPane.showMessageDialog(null, "Error al acceder a la tabla alumno");
+
+        }
+    }
+    
+    public void activarlector(int id){
+            
+            String sql = "UPDATE lector SET estado = 1 WHERE idLector = ?";
+            
+            
+        try {
+            PreparedStatement ps = con.prepareStatement(sql);
+            
+            ps.setInt(1,id);
+            
+            int exito = ps.executeUpdate();
+            
+            if (exito == 1 ){
+                
+               JOptionPane.showMessageDialog(null, "Lector Activo");
+                
+                
+            }
+       
+        } catch (SQLException ex) {
+             JOptionPane.showMessageDialog(null, "Error al acceder a la tabla Lector");
+        }
+            
+        }
+    
+     public void desactivarlector(int id){
+            
+            String sql = "UPDATE lector SET estado = 0 WHERE idLector = ?";
+            
+            
+        try {
+            PreparedStatement ps = con.prepareStatement(sql);
+            
+            ps.setInt(1,id);
+            
+            int exito = ps.executeUpdate();
+            
+            if (exito == 1 ){
+                
+               JOptionPane.showMessageDialog(null, "Lector Desactivo");
+                    
+            }
+       
+        } catch (SQLException ex) {
+             JOptionPane.showMessageDialog(null, "Error al acceder a la tabla Lector");
+        }
+            
+        }
+     
+      public Lector buscarlector(int id){
+            
+            String sql = "SELECT dni, nombre, domicilio, telefono, estado FROM lector WHERE IdLector = ? AND estado = 1";
+       
+        Lector lector = null;
+        try {
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setInt(1, id);
+            ResultSet rs = ps.executeQuery();
+            
+            if(rs.next()){
+                
+              lector = new Lector ();  
+              
+              lector.setIdLector(id);
+              lector.setDni(rs.getInt("dni"));
+              lector.setNombre(rs.getString("nombre"));
+              lector.setDomicilio(rs.getString("nombre"));
+              lector.setTelefono(rs.getInt("telefono"));
+              lector.setEstado(true);
+              
+            }else{
+                
+                JOptionPane.showMessageDialog(null, "No existe ese lector");
+            }
+            
+        } catch (SQLException ex) {
+             JOptionPane.showMessageDialog(null, "Error al acceder a la tabla lector");
+        }
+        
+        return lector;
+        }
+      
+      
+      
+       public List<Lector> listarLectoresPrestamo(){
+            
+           
+             String sql = "SELECT l.idLector, l.nombre, l.domicilio, l.telefono "
+                + "FROM prestamo p "
+                + "JOIN lector l ON p.idLector = l.idLector "
+                + "WHERE p.estado = 1 AND l.estado = 1" ;
+            
+        ArrayList<Lector> lectores = new ArrayList<>();
+        
+        try {
+            PreparedStatement ps = con.prepareStatement(sql);
+           
+            ResultSet rs = ps.executeQuery();
+            
+            while(rs.next()){
+                
+              Lector lector = new Lector ();  
+              
+              lector.setIdLector(rs.getInt("idLector"));
+              lector.setDni(rs.getInt("dni"));
+              lector.setNombre(rs.getString("nombre"));
+              lector.setTelefono(rs.getInt("telefono"));
+              lector.setEstado(true);
+              
+              lectores.add(lector);
+              
+            }
+            ps.close();
+            
+        } catch (SQLException ex) {
+             JOptionPane.showMessageDialog(null, "Error al acceder a las tablas");
+        }
+        
+        return lectores;
+        }
+        
+}
+
+
